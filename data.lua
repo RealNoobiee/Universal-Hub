100ae5bd5857caded1950690132284613d0907e0
learningaboutcode <sulthanabdillah8@gmail.com>
2025-05-05 17:26:39 +0700

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,16 @@
+{
+  "scripts": [
+    {
+      "name": "Infinite Yield",
+      "code": "loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()"
+    },
+    {
+      "name": "DEX Explorer",
+      "code": "loadstring(game:HttpGet('https://raw.githubusercontent.com/peyton2465/DEX-Explorer/main/DEX%20V2'))()"
+    },
+    {
+      "name": "Remote Spy",
+      "code": "loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpyBeta.lua'))()"
+    }
+  ]
+}
